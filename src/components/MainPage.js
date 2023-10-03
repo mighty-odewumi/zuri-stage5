9d382d6148db36a1495f9a66e69c5476faaae22d
gr1ntch <odewumimighty@gmail.com>
2023-10-03 23:19:54 +0100

    Added footer and email popup

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,68 @@
+import { useState } from "react"
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
 export default function MainPage() {
+
+  const [isSend, setIsSend] = useState(false);
+
+  function handleSend(e) {
+    e.preventDefault();
+    setIsSend(true);
+  }
+
+  function handleClose(e) {
+    setIsSend(false);
+  }
+
+  console.log(isSend);
+
+  const emailPopupStyles = {
+    display: `${isSend ? "block" : "none"}`,
+  };
+
+  const mainPageStyles = {
+    filter: `${isSend ? "opacity(0.4)" : ""}`,
+    // overflow: `${isSend ? "hidden" : ""}`
+  };
+
+  const emailPopup = (
+    <div
+      className="email-popup"
+      style={emailPopupStyles}
+    >
+      
+      <img src="assets/close-circle.svg" alt="" className="close-icon" onClick={handleClose}/>
+
+      <img src="assets/success-kite.svg" alt="success kite" className="success-kite" />
+
+      <p className="success-text">
+        Your video link has been sent to&nbsp; 
+        <span className="email">
+          johnmark@gmail.com
+        </span>
+      </p>
+
+      <p>Would you need to view this video later? Save to your account now!</p>
+
+      <button>Save Video</button>
+
+      <p className="signup-text">Don't have an account? 
+        <a href="#">
+          <span>
+          Create account
+          </span>
+        </a>
+      </p>
+
+    </div>
+  )
+
   return (
     <>
-      <div className="page-wrapper">
+      <div className="page-wrapper"
+        style={mainPageStyles}
+      >
+
         <header>
           <nav>
             <div className="main-logo">
@@ -28,7 +89,7 @@ export default function MainPage() {
               <img src="assets/edit.svg" alt="edit icon" />
             </div>
 
-            <form>
+            <form onClick={handleSend}>
               <input type="email" placeholder="Enter email of receiver"/>
               <button>
                 Send
@@ -69,7 +130,6 @@ export default function MainPage() {
             </div>
           </section>
 
-
           <section className="video-transcript">
             <video controls />
 
@@ -83,31 +143,26 @@ export default function MainPage() {
             <div className="transcripts">
               <div className="transcript">
                 <span>0.01</span>
-                <p>This is a translation of the highest order</p>
+                <p>This is a translation of the highest order. This is a translation of the highest order. This is a translation of the highest order.</p>
               </div>
               
               <div className="transcript">
                 <span>0.03</span>
-                <p>This is a translation of the highest order</p>
-              </div>
-
-              <div className="transcript">
-                <span>0.04</span>
-                <p>This is a translation of the highest order</p>
+                <p>This is a translation of the highest order. This is a translation of the highest order. This is a translation of the highest order.</p>
               </div>
 
               <div className="transcript">
                 <span>0.05</span>
-                <p>This is a translation of the highest order</p>
+                <p>I understand this is just a transition from one greatness to another and I would have it no other way than to do something like this.</p>
               </div>
 
               <div className="transcript">
-                <span>0.05</span>
-                <p>This is a translation of the highest order</p>
+                <span>0.07</span>
+                <p>It is a pity Lorem Ipsum didn't exist until this present age in the history of the world and that is how we have come to view them.</p>
               </div>
 
               <div className="transcript">
-                <span>0.06</span>
+                <span>0.08</span>
                 <p>This is a translation of the highest order</p>
               </div>
             </div>
@@ -124,7 +179,7 @@ export default function MainPage() {
             </div>
             <video controls className="mobile"/>
 
-            <form>
+            <form onClick={handleSend}>
               <input type="email" placeholder="Enter email of receiver"/>
               <button>
                 Send
@@ -170,31 +225,26 @@ export default function MainPage() {
             <div className="transcripts">
               <div className="transcript">
                 <span>0.01</span>
-                <p>This is a translation of the highest order</p>
+                <p>This is a translation of the highest order. This is a translation of the highest order. This is a translation of the highest order.</p>
               </div>
               
               <div className="transcript">
                 <span>0.03</span>
-                <p>This is a translation of the highest order</p>
-              </div>
-
-              <div className="transcript">
-                <span>0.04</span>
-                <p>This is a translation of the highest order</p>
+                <p>This is a translation of the highest order. This is a translation of the highest order. This is a translation of the highest order.</p>
               </div>
 
               <div className="transcript">
                 <span>0.05</span>
-                <p>This is a translation of the highest order</p>
+                <p>I understand this is just a transition from one greatness to another and I would have it no other way than to do something like this.</p>
               </div>
 
               <div className="transcript">
-                <span>0.05</span>
-                <p>This is a translation of the highest order</p>
+                <span>0.07</span>
+                <p>It is a pity Lorem Ipsum didn't exist until this present age in the history of the world and that is how we have come to view them.</p>
               </div>
 
               <div className="transcript">
-                <span>0.06</span>
+                <span>0.08</span>
                 <p>This is a translation of the highest order</p>
               </div>
             </div>
@@ -220,8 +270,46 @@ export default function MainPage() {
 
         </section>
 
+        <footer>
+          <div className="main-logo">
+            <img src="assets/help_logo.svg" alt="logo" />
+            <h3>HelpMeOut</h3>
+          </div>
+
+          <div className="footer-sections">
+            <span>Menu</span>
+
+            <a href="#">Home</a>
+            <a href="#">Converter</a>
+            <a href="#">How it Works</a>
+          </div>
+
+          <div className="footer-sections">
+            <span>About us</span>
+
+            <a href="#">About</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Privacy Policy</a>
+          </div>
+
+          <div className="footer-sections">
+            <span>Screen Record</span>
+
+            <a href="#">Browser Window</a>
+            <a href="#">Desktop</a>
+            <a href="#">Application</a>
+          </div>
+
+        </footer>
+
       </div>
-    </>
-    
+
+      {
+        isSend 
+          ? emailPopup
+          : ""
+      }
+
+    </>   
   )
 }
